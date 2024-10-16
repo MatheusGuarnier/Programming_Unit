903b7ac6ae5e7537de1a140656787975c6cf0226
Matheus Guarnier (s5720322) <s5720322@bournemouth.ac.uk>
2024-10-16 10:52:21 +0100

    Lab 3

---
+++ Lists_type.py
@@ -0,0 +1,12 @@
+my_list = [15, 23, 32, 46]
+
+print(my_list[0])
+print("")
+print(my_list[-1])
+print("")
+print(my_list[:2])
+
+result = my_list[0] + my_list[-1]
+my_list.append(result)
+
+print(my_list)
---
+++ Sequence_types.py
@@ -0,0 +1,58 @@
+#Task 1 Lists
+your_list = [200, 50, "Python", 3 + 6, "Lab 3"]
+
+print("") #Task 2
+
+print(len(your_list))
+
+print("") #Task 3
+
+your_list.append(3.50)
+print(your_list)
+
+print("") #Task 4
+
+your_list.insert(0, "OOP")
+print(your_list)
+
+print("") #Task 5
+
+your_list.remove(3.5)
+print(your_list)
+
+print("") #Task 6
+
+print(len(your_list))
+
+print("") #Task 7
+
+print(your_list[3])
+
+print("") #Task 8
+
+your_list[1] = 1000
+print(your_list)
+
+print("") #Task 9
+
+print(type(your_list))
+
+print("") #Task 10 Tuples
+
+your_tuple = tuple(your_list)
+print(your_tuple)
+
+print("") #Task 11
+
+print(type(your_tuple))
+
+print("") #Task 12
+
+print(your_tuple[0])
+
+print("") #Task 13
+
+print(len(your_tuple))
+
+your_tuple[0] = 5
+print(your_tuple)
